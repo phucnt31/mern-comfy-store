d4f82db36b06d4965022cf1b4bf78345adec3587
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-05 11:06:47 +0700

    Customize SubmitBtn.jsx

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigation } from "react-router-dom";
 
-const SubmitBtn = () => {
-  return <div>SubmitBtn</div>;
+const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <button
+      type="submit"
+      disabled={isSubmitting}
+      className="btn btn-primary btn-block"
+    >
+      {isSubmitting ? (
+        <span className="loading loading-spinner">sending...</span>
+      ) : (
+        text || "submit"
+      )}
+    </button>
+  );
 };
 
 export default SubmitBtn;
