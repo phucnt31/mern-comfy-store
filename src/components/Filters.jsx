8849f041f093bc80dcee1de06213c98c3d401bbe
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 20:45:39 +0700

    Add price and shipping filter to Filters.jsx

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Form, Link, useLoaderData } from "react-router-dom";
 import FormInput from "./FormInput";
 import FormSelect from "./FormSelect";
+import FormRange from "./FormRange";
+import FormCheckbox from "./FormCheckbox";
 
 const Filters = () => {
   const { meta } = useLoaderData();
@@ -36,6 +38,10 @@ const Filters = () => {
         list={["a-z", "z-a", "high", "low"]}
         size="select-sm"
       />
+      {/* PRICE */}
+      <FormRange label="select price" name="price" size="range-sm" />
+      {/* SHIPPING */}
+      <FormCheckbox label="free shipping" name="shipping" size="checkbox-sm" />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm ">
         search
