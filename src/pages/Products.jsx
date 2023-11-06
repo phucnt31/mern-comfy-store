fdf347ac27a600b626994439cd19ed30060c0418
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 20:59:59 +0700

    Add filter condition to loader in Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { customFetch } from "../utils";
 import { Filters, PaginationContainer, ProductsContainer } from "../components";
 
-export const productsLoader = async () => {
-  const resp = await customFetch.get("/products");
-  return { products: resp.data.data, meta: resp.data.meta };
+export const productsLoader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const resp = await customFetch.get("/products", { params });
+  return { products: resp.data.data, meta: resp.data.meta, params };
 };
 
 const Products = () => {
