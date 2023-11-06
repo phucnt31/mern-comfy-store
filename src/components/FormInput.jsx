497ce5e7fec708d956821fb96affd1b7f1569759
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 20:34:57 +0700

    Add size prop and customize Filters.jsx

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FormInput = ({ label, name, type, defaultValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <div className="form-control">
       <label htmlFor={name} className="label">
@@ -11,7 +11,7 @@ const FormInput = ({ label, name, type, defaultValue }) => {
         name={name}
         id={name}
         defaultValue={defaultValue}
-        className="input input-bordered"
+        className={`input input-bordered ${size}`}
       />
     </div>
   );
