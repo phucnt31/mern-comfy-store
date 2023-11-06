922d66e3c6268582b37b8064a7a53ee0e45094cf
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 19:53:49 +0700

    Create formatPrice function and use it in ProductsGrid.jsx

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -5,3 +5,11 @@ const productionUrl = "https://strapi-store-server.onrender.com/api";
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
+
+export const formatPrice = (amount) => {
+  const dollarAmount = new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD",
+  }).format((amount / 100).toFixed(2));
+  return dollarAmount;
+};
