1ef2311a8b11c5446c06c672fd8d155e2cf2c685
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 19:22:00 +0700

    Customize Hero component and import to Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Hero } from "../components";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Landing;
