e3698843f4611a3158aa6cf83a0a82be1c29a808
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-06 19:13:47 +0700

    Add NavLinks component to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const themes = {
   winter: "winter",
@@ -40,17 +41,19 @@ const Navbar = () => {
           <div className="dropdown">
             <label className="btn btn-ghost lg:hidden" tabIndex={0}>
               <FaBarsStaggered />
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-              >
-                nav links
-              </ul>
             </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+            >
+              <NavLinks />
+            </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal ">nav links</ul>
+          <ul className="menu menu-horizontal ">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* THEME ICONS */}
