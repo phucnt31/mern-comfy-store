076075531000348fdf4c2d36439e52b879cf87d7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-07 20:46:19 +0700

    Setup Logout and Access User

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const links = [
@@ -11,13 +12,16 @@ const links = [
 ];
 
 const NavLinks = () => {
+  const user = useSelector((state) => state.userState.user);
+
   return (
     <>
       {links.map((link) => {
         const { id, url, text } = link;
+        if ((url === "checkout" || url === "orders") && !user) return null;
         return (
           <li key={id}>
-            <NavLink to={url} className="capitalize">
+            <NavLink className="capitalize" to={url}>
               {text}
             </NavLink>
           </li>
