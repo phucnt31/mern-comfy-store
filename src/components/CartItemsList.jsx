573edbb20192967575fb3e3d981aba8271536b85
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-07 20:12:37 +0700

    Customize Cart.jsx and create 3 components and export them

---
+++ src/components/CartItemsList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+const CartItemsList = () => {
+  return (
+    <>
+      <CartItem />
+    </>
+  );
+};
+
+export default CartItemsList;
