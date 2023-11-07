352bff2be0f86abea2688ecdfbec3db10c277ff6
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-07 20:59:37 +0700

    Create CheckoutForm component and setup Checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal.length === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8  md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 
 export default Checkout;
