b59e92ada79adf68143d5c8f3e6675d301869f94
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-07 20:56:42 +0700

    Add Demo User functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,6 +28,24 @@ export const loginAction =
   };
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", {
+        identifier: "test@test.com",
+        password: "secret",
+      });
+      dispatch(loginUser(response.data));
+      toast.success("welcome guest user");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("guest user login error.please try later.");
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -35,22 +53,16 @@ const Login = () => {
         method="post"
       >
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput
-          label="email"
-          type="email"
-          defaultValue="test@test.com"
-          name="identifier"
-        />
-        <FormInput
-          label="password"
-          type="password"
-          defaultValue="secret"
-          name="password"
-        />
+        <FormInput label="email" type="email" name="identifier" />
+        <FormInput label="password" type="password" name="password" />
         <div className="mt-4">
           <SubmitBtn text="login" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block">
+        <button
+          type="button"
+          onClick={loginAsGuestUser}
+          className="btn btn-secondary btn-block"
+        >
           guest user
         </button>
         <p className="text-center">
