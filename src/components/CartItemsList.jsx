8ed99b6d0ac29b1d75fdac4caae4318a32c4cbf9
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-07 20:15:36 +0700

    Customize CartItem.jsx and CartItemsList.jsx

--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 
 const CartItemsList = () => {
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
   return (
-    <>
-      <CartItem />
-    </>
+    <div>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </div>
   );
 };
-
 export default CartItemsList;
