d7499be056dcce6b1ea6de6be65927b91ca17ed0
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-16 19:26:09 +0700

    Admin user

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,6 +4,11 @@ const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
 });
 
 export default mongoose.model("User", UserSchema);
