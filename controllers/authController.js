ad1921faee777dfb6b4888ba3028533b91f449e1
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-16 19:48:47 +0700

    Hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,13 @@
 import { StatusCodes } from "http-status-codes";
 import UserModel from "../models/UserModel.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
+  const { password } = req.body;
   const isFirstAccount =
     (await UserModel.countDocuments()) === 0 ? "admin" : "user";
   req.body.role = isFirstAccount;
+  req.body.password = await hashPassword(password);
   const user = await UserModel.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
