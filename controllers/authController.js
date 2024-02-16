221fb8bee5068ca12fecfe8cfc37abd17133ad4d
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-16 20:30:20 +0700

    Login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import UserModel from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthorizedError } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
   const { password } = req.body;
@@ -8,10 +9,23 @@ export const register = async (req, res) => {
     (await UserModel.countDocuments()) === 0 ? "admin" : "user";
   req.body.role = isFirstAccount;
   req.body.password = await hashPassword(password);
-  const user = await UserModel.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  await UserModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "User created" });
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  const user = await UserModel.findOne({ email: req.body.email });
+  if (!user) {
+    throw new UnauthorizedError("Invalid credentials");
+  }
+
+  const isCorrectPassword = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isCorrectPassword) {
+    throw new UnauthorizedError("Incorrect password");
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "Login successfully" });
 };
