538508149346e43234e7324a4d256a362fcdbff9
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-16 09:18:14 +0700

    Accept JSON

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
 });
 
 app.listen(5200, () => {
