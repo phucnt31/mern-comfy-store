5e257f94a7ba5f0d556934c92814aa73955f89cd
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-17 12:55:53 +0700

    Add order detail model

---
+++ models/OrderDetailModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const OrderDetailSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  cartID: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
+  price: {
+    type: Number,
+    required: true,
+  },
+  productColor: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  productID: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+export default mongoose.model("OrderDetail", OrderDetailSchema);
